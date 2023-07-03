0af73146f8fb8a80872f2da9221e6fd2b1d63274
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2023-07-03 10:55:08 +0200

    Integrate get grid layout logic
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/layout/explore/GridItem.tsx
+++ web/src/layout/explore/GridItem.tsx
@@ -1,67 +1,3 @@
-// import classNames from 'classnames';
-// import { BaseItem } from '../../types';
-// import styles from './GridItem.module.css';
-// import DropdownOnHover from '../common/DropdownOnHover';
-
-// interface Props {
-//   item: BaseItem;
-//   onClick: (item: BaseItem) => void;
-// }
-
-// const GridItem = (props: Props) => {
-//   return (
-//     <DropdownOnHover
-//       width={350}
-//       dropdownClassName={styles.dropdown}
-//       className={classNames(
-//         'card rounded-0 position-relative p-0',
-//         styles.card,
-//         { [styles.bigCard]: props.item.featured },
-//         { [styles.noRepo]: !props.item.has_repositories }
-//       )}
-//       linkContent={
-//         <button className={`btn w-100 h-100 ${styles.cardContent}`} onClick={() => props.onClick(props.item)}>
-//           <img
-//             alt={`${props.item.name} logo`}
-//             className={`m-auto ${styles.logo}`}
-//             src={import.meta.env.MODE === 'development' ? `../../static/${props.item.logo}` : `${props.item.logo}`}
-//           />
-//           {props.item.featured && props.item.featured.label && (
-//             <div
-//               className={`text-center text-uppercase text-dark border-top position-absolute start-0 end-0 bottom-0 ${styles.legend}`}
-//             >
-//               {props.item.featured.label}
-//             </div>
-//           )}
-//         </button>
-//       }
-//       tooltipStyle
-//     >
-//       <div className="p-2">
-//         <div className="d-flex flex-row">
-//           <div className={`d-flex align-items-center justify-content-center me-3 ${styles.dropdownLogoWrapper}`}>
-//             <img
-//               alt={`${props.item.name} logo`}
-//               className={`m-auto ${styles.dropdownLogo}`}
-//               src={import.meta.env.MODE === 'development' ? `../../static/${props.item.logo}` : `${props.item.logo}`}
-//             />
-//           </div>
-
-//           <div className={`flex-grow-1 p-3 ${styles.itemInfo}`}>
-//             <div className="fw-semibold">{props.item.name}</div>
-//             <div>{props.item.project && <div className="badge rounded-0 bg-primary">CNCF</div>}</div>
-//           </div>
-//         </div>
-//         <div className={`mt-2 text-muted ${styles.description}`}>
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-//         </div>
-//       </div>
-//     </DropdownOnHover>
-//   );
-// };
-
-// export default GridItem;
-
 import { useEffect, useRef, useState } from 'react';
 
 import { useOutsideClick } from '../../hooks/useOutsideClick';
@@ -82,8 +18,8 @@ const DEFAULT_DROPDOWN_WIDTH = 350;
 const DEFAULT_MARGIN = 30;
 
 const GridItem = (props: Props) => {
-  const ref = useRef(null);
-  const wrapper = useRef<HTMLDivElement | null>(null);
+  const ref = useRef<HTMLDivElement>(null);
+  const wrapper = useRef<HTMLDivElement>(null);
   const [itemInfo, setItemInfo] = useState<Item | null | undefined>(undefined);
   const [openStatus, setOpenStatus] = useState(false);
   const [onLinkHover, setOnLinkHover] = useState(false);
@@ -162,7 +98,7 @@ const GridItem = (props: Props) => {
 
   return (
     <div
-      style={props.item.featured ? { border: `1px solid ${props.borderColor}` } : {}}
+      style={props.item.featured ? { border: `2px solid ${props.borderColor}` } : {}}
       className={classNames(
         'card rounded-0 position-relative p-0',
         styles.card,
@@ -218,7 +154,7 @@ const GridItem = (props: Props) => {
           {props.item.featured && props.item.featured.label && (
             <div
               className={`text-center text-uppercase text-dark position-absolute start-0 end-0 bottom-0 ${styles.legend}`}
-              style={props.item.featured ? { borderTop: `1px solid ${props.borderColor}` } : {}}
+              style={props.item.featured ? { borderTop: `2px solid ${props.borderColor}` } : {}}
             >
               {props.item.featured.label}
             </div>
