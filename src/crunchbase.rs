c7c3aeff0cc8df199fc4f1d196b5e82932265bcb
Sergio Castaño Arteaga <tegioz@icloud.com>
2023-07-07 11:15:02 +0200

    Fix issue preparing logos from url
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- src/crunchbase.rs
+++ src/crunchbase.rs
@@ -1,4 +1,4 @@
-//! This modules defines some types used to represent the information collected
+//! This module defines some types used to represent the information collected
 //! from Crunchbase for each of the landscape items (when applicable), as well
 //! as the functionality used to collect that information.
 
