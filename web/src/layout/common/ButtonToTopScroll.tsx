cncf/landscape2

commit 44168b87c23fd9e3e7d7d56beaf60997ae1f6631

Fix issue with button to scroll to the top (#285)
Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

web/src/layout/common/ButtonToTopScroll.module.css
@@ -1,5 +1,5 @@
 .sticky {
-  bottom: 1.5rem;
+  bottom: 2.5rem;
 }
 
 .btnTop {

web/src/layout/common/ButtonToTopScroll.tsx
@@ -1,7 +1,6 @@
-import { useWindowScrollPosition } from '@solid-primitives/scroll';
 import { useNavigate } from '@solidjs/router';
 import isUndefined from 'lodash/isUndefined';
-import { createEffect, createSignal, Show } from 'solid-js';
+import { createSignal, Show } from 'solid-js';
 
 import { SVGIconKind } from '../../types';
 import isElementInView from '../../utils/isElementInView';
@@ -12,17 +11,9 @@ interface Props {
   firstSection: string | null;
 }
 
-const NAV_HEIGHT = 200;
-
 const ButtonToTopScroll = (props: Props) => {
   const navigate = useNavigate();
-  const [isVisible, setIsVisible] = createSignal<boolean>(false);
-  const scroll = useWindowScrollPosition();
-  const y = () => scroll.y;
-
-  createEffect(() => {
-    setIsVisible(y() > NAV_HEIGHT);
-  });
+  const [isVisible] = createSignal<boolean>(true);
 
   return (
     <Show when={!isUndefined(props.firstSection)}>

web/src/layout/explore/card/index.tsx
@@ -176,7 +176,7 @@ const CardCategory = (props: Props) => {
             <Show when={fullDataReady()}>
               <Content menu={menu} data={props.data} isVisible={props.initialIsVisible} />
             </Show>
-            <Show when={!isUndefined(firstItem())}>
+            <Show when={!isUndefined(firstItem()) && fullDataReady()}>
               <ButtonToTopScroll firstSection={firstItem()!} />
             </Show>
           </div>

commit 0b2a1f037a3126c5a210f7fd857fe90fbdcb7bb0

Fix regression in web application introduced in #285
Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

web/index.html
@@ -2,15 +2,18 @@
 <html lang="en" data-theme="light">
   <head>
     <meta charset="UTF-8" />
-    {% if datasets.base.images.favicon.is_some() %}
-    <link rel="shortcut icon" href="{{ datasets.base.images.favicon.as_ref().unwrap() }}" />
-    <link rel="apple-touch-icon" href="{{ datasets.base.images.favicon.as_ref().unwrap() }}" />
-    {% endif %}
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    {% if datasets.base.images.open_graph.is_some() %}
-    <meta property="og:image" content="{{ datasets.base.images.open_graph.as_ref().unwrap() }}">
-    <meta name="twitter:image:src" content="{{ datasets.base.images.open_graph.as_ref().unwrap() }}" />
-    {% endif %}
+    <% if (!isDev) { %>
+      {% if datasets.base.images.favicon.is_some() %}
+      <link rel="shortcut icon" href="{{ datasets.base.images.favicon.as_ref().unwrap() }}" />
+      <link rel="apple-touch-icon" href="{{ datasets.base.images.favicon.as_ref().unwrap() }}" />
+      {% endif %}
+
+      {% if datasets.base.images.open_graph.is_some() %}
+      <meta property="og:image" content="{{ datasets.base.images.open_graph.as_ref().unwrap() }}">
+      <meta name="twitter:image:src" content="{{ datasets.base.images.open_graph.as_ref().unwrap() }}" />
+      {% endif %}
+    <% } %>
     <title>{{ datasets.base.foundation }} Landscape</title>
     <% if (isDev) { %>
     <script>

web/src/layout/common/ButtonToTopScroll.module.css
@@ -1,5 +1,5 @@
 .sticky {
-  bottom: 2.5rem;
+  bottom: 1.5rem;
 }
 
 .btnTop {

web/src/layout/common/ButtonToTopScroll.tsx
@@ -29,7 +29,7 @@ const ButtonToTopScroll = (props: Props) => {
                   replace: true,
                 });
 
-                window.scrollTo({
+                document.getElementById('landscape')!.scrollTo({
                   top: 0,
                   left: 0,
                   behavior: 'instant',

web/src/layout/explore/card/Menu.module.css
@@ -10,8 +10,6 @@
 
 .content {
   overscroll-behavior: contain;
-  flex-grow: 1;
-  margin-bottom: 2rem;
 }
 
 .truncateWrapper {

web/src/layout/explore/card/Menu.tsx
@@ -43,7 +43,7 @@ const Menu = (props: Props) => {
 
   return (
     <div class={`d-flex flex-column me-4 sticky-top ${styles.toc}`}>
-      <div id="menu" classList={{ [`overflow-y-auto ${styles.content}`]: offsetActive() }}>
+      <div id="menu" classList={{ [`offcanvas-body ${styles.content}`]: offsetActive() }}>
         <div ref={setRef}>
           <For each={Object.keys(props.menu())}>
             {(cat, index) => {