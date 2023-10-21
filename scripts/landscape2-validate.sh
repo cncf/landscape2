eaf856ba604c11a044d5f23c714595fbdd6db2ad
Sergio Castaño Sánchez <sergiocastanosanchez@icloud.com>
2023-10-21 11:37:11 +0200

    Add entrypoint validation script
    Signed-off-by: Sergio Castaño Sánchez <sergiocastanosanchez@icloud.com>

---
+++ scripts/landscape2-validate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+TARGET_KIND=$1
+TARGET_PATH=$2
+
+function printUsage() {
+    echo "USAGE: landscape2-validate.sh <TARGET> <PATH>"
+    echo -e "\nTARGET options: data, guide or settings"
+    exit 1
+}
+
+if [ -z "$TARGET_KIND" ] || [ -z "$TARGET_PATH" ]; then
+    printUsage
+fi
+
+case $TARGET_KIND in
+  data)
+    landscape2 validate data --data-file $TARGET_PATH
+    ;;
+  guide)
+    landscape2 validate guide --guide-file $TARGET_PATH
+    ;;
+  settings)
+    landscape2 validate settings --settings-file $TARGET_PATH
+    ;;
+  *)
+    printUsage
+    ;;
+esac
