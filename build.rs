3aa58b6ce927f0454b8c53c3191f09d3f97db3c6
Sergio Castaño Arteaga <tegioz@icloud.com>
2023-10-30 13:02:19 +0100

    Fix label style issue
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- build.rs
+++ build.rs
@@ -1,19 +1,11 @@
 use anyhow::{format_err, Result};
 use std::process::{Command, Output};
-use which::which;
 
 fn main() -> Result<()> {
     // Tell Cargo to rerun this build script if the web app changes
     println!("cargo:rerun-if-changed=web/src");
     println!("cargo:rerun-if-changed=web/static");
     println!("cargo:rerun-if-changed=web/index.html");
-
-    // Check if required external tools are available
-    if which("yarn").is_err() {
-        return Err(format_err!(
-            "yarn not found in PATH (it is required to build the web application)"
-        ));
-    }
 
     // Build web application
     let error = |cmd: &str, output: Output| {
