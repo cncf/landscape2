2bef1ba475a34f807fdc227f8dc15d87700e46de
Cintia Sánchez García <cynthiasg@icloud.com>
2024-01-01 14:15:20 +0100

    Add iframe-resizer support to embeddable view (#424)
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- embed/vite.config.ts
+++ embed/vite.config.ts
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 
 export default defineConfig({
   base: '/embed',
@@ -10,5 +12,14 @@ export default defineConfig({
       },
     },
   },
-  plugins: [solid()],
+  plugins: [solid(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './src/assets/js/iframeResizer.contentWindow-v4.3.9.min.js',
+          dest: 'assets'
+        }
+      ]
+    })
+  ]
 });
