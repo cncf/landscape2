885af33df61c367a36289559f06b83ea419281f0
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2024-02-09 13:48:17 +0100

    Fix linter
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/window.d.ts
+++ web/src/window.d.ts
@@ -4,6 +4,6 @@ declare global {
   interface Window {
     baseDS: BaseData;
     statsDS: Stats;
-    Osano: { cm: { showDrawer: (t: string) => void, addEventListener: (t: string, f: unknown) => void } };
+    Osano: { cm: { showDrawer: (t: string) => void; addEventListener: (t: string, f: unknown) => void } };
   }
 }
