26877849f38ac782d1c4552a6fd59e1493352b01
Sergio Castaño Arteaga <tegioz@icloud.com>
2026-03-06 08:28:46 +0100

    Revert reqwest-middleware upgrade
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- crates/cli/src/build/github.rs
+++ crates/cli/src/build/github.rs
@@ -212,7 +212,11 @@ impl GHApi {
     fn new(token: &str) -> Result<Self> {
         // Setup octorust GitHub API client
         let user_agent = format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
-        let gh_client = octorust::Client::new(user_agent.clone(), Credentials::Token(token.to_string()))?;
+        let gh_client = octorust::Client::custom(
+            user_agent.clone(),
+            Credentials::Token(token.to_string()),
+            reqwest_middleware::ClientBuilder::new(reqwest::Client::builder().build()?).build(),
+        );
 
         // Setup HTTP client ready to make requests to the GitHub API
         // (for some operations that cannot be done with the octorust client)
